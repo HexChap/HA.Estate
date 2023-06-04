88b86eaba2acd84f14ac37efce3c784af7478b23
Kirill Ibragimov <hexchap@gmail.com>
2023-06-04 17:19:53 +0300

    ✅  ImageEditor's methods are now async. ImageEditor._save_image method is done.

--- editor.py
+++ editor.py
@@ -1,8 +1,20 @@
-class ImageEditor:
-    def __init__(self):
-        pass
+from io import BytesIO
+from pathlib import Path
 
-    def add_logo(self):
+import aiofiles
+from PIL import Image, ImageOps
+
+
+class ImageEditor:
+    def __init__(
+            self,
+            in_paths: list[Path],
+            out_path: Path | str
+    ):
+        self.images = [Image.open(path) for path in in_paths]
+        self.output_path: Path = Path(out_path)
+
+    async def add_logo(self):
         pass
 
     def rename_images(self):
@@ -19,6 +31,9 @@ class ImageEditor:
     def _calc_logo_width():
         pass
 
-    @staticmethod
-    def _save_image():
-        pass
+    async def _save_image(self, image: Image.Image):
+        image.save(buf := BytesIO(), format="JPEG")
+        path = self.output_path / Path(image.filename).name
+
+        async with aiofiles.open(path, "wb") as file:
+            await file.write(buf.getbuffer())
