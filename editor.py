605aed7f388f145f80a1a8632b050ceec645eee3
Kirill Ibragimov <hexchap@gmail.com>
2023-06-04 16:43:47 +0300

    ✨  Project restarted. Images Editor's architecture prepared. utils.get_images done

---
+++ editor.py
@@ -0,0 +1,24 @@
+class ImageEditor:
+    def __init__(self):
+        pass
+
+    def add_logo(self):
+        pass
+
+    def rename_images(self):
+        pass
+
+    def compress_images(self):
+        pass
+
+    @staticmethod
+    def _resize_with_proportion():
+        pass
+
+    @staticmethod
+    def _calc_logo_width():
+        pass
+
+    @staticmethod
+    def _save_image():
+        pass
